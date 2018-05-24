7d1e0018a133a8ef0c0ece7340c03e015aad864a
= <graziettahof@gmail.com>
2018-05-24 03:10:41 +0000

    added requested changes for status summary pull request

--- src/status/helpers.spec.js
+++ src/status/helpers.spec.js
@@ -3,25 +3,25 @@ const assert = require('assert');
 const {
     occuredWithinCurrentWeek,
     occuredWithinCurrentMonth
-} = require('./helpers')
+} = require('./helpers');
 
 describe('status helpers', function () {
     describe('occuredWithinCurrentWeek', function () {
         it('should return true given a date within the current week', function () {
-            const date_today = moment().toDate()
-            assert(occuredWithinCurrentWeek(date_today))
+            const date_today = moment().toDate();
+            assert(occuredWithinCurrentWeek(moment(), date_today));
         });
         it('should return false given a date not within the current week', function () {
-            assert(!occuredWithinCurrentWeek('20170301'))
+            assert(!occuredWithinCurrentWeek(moment(), '20170301'))
         });
     })
     describe('occuredWithinCurrentMonth', function () {
         it('should return true given a date within the current month', function () {
             const date_today = moment().toDate()
-            assert(occuredWithinCurrentMonth(date_today))
+            assert(occuredWithinCurrentMonth(moment(), date_today))
         });
         it('should return false given a date not within the current month', function () {
-            assert(!occuredWithinCurrentMonth('20170301'))
+            assert(!occuredWithinCurrentMonth(moment(), '20170301'))
         });
     })
 });
